f3206786c9ee4b39f7a08dc3d0464e352ba189a5
khemmika <khemmika.ph.68@ubu.ac.th>
2026-02-13 17:24:43 +0700

    อ๊ากกกกก

--- week10/log_viewer/interfaces/data_source.py
+++ week10/log_viewer/interfaces/data_source.py
@@ -1,10 +1,10 @@
+# interface ILogSource(ABC):
 from abc import ABC, abstractmethod
 from typing import List
 
-# Interface (Contract)
+
 class ILogSource(ABC):
-    
     @abstractmethod
     def get_logs(self) -> List[str]:
-        """ดึงข้อมูล Logs กลับมาเป็น List ของ String"""
+        """Retrieve logs from the source."""
         pass
--- week10/log_viewer/services/file_source.py
+++ week10/log_viewer/services/file_source.py
@@ -1,14 +1,10 @@
-from ast import List
-from week10.log_viewer.interfaces.data_source import ILogSource
+from interfaces.data_source import ILogSource
 
 
 class FileLogSource(ILogSource):
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self, file_path: str):
+        self.file_path = file_path
 
-    def get_logs(self) -> List[str]:
-        try:
-            with open(self.filepath, 'r') as f:
-                return f.readlines()
-        except FileNotFoundError:
-            return ["Error: File not found"]
+    def get_logs(self) -> list[str]:
+        with open(self.file_path, 'r') as file:
+            return file.read().split('\n')
